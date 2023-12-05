97263dc93ffb3d62541c4bd426891aa0cec65d39
Enrico Zappalà <zpp.enrico@gmail.com>
2023-12-05 12:04:00 +0100

    Add button & fetch products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
+import Product from './Product.jsx';
+import useFetch from './useFetch.jsx';
+
 export default function Products(props) {
+	const [products, setProducts] = useState([]);
+	const { get } = useFetch(
+		'https://simple-store-database-default-rtdb.europe-west1.firebasedatabase.app/'
+	);
+
+	useEffect(() => {
+		get('products.json')
+			.then((data) => {
+				setProducts(data);
+			})
+			.catch((error) => console.log('Could not load products', error));
+	}, []);
+
 	return (
 		<div className='products-layout'>
 			<h1>Products</h1>
 			<p>Take a look at our products</p>
+			<div className='products-grid'>
+				{products.map((product) => {
+					return <Product key={product.id} details={product} />;
+				})}
+			</div>
 		</div>
 	);
 }
