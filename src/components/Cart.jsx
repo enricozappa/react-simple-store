a3ef440653499860bbd84d8fca3229b23d677c8b
Enrico Zappalà <zpp.enrico@gmail.com>
2023-12-14 09:22:24 +0100

    Add UI Input component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,5 @@
+import Input from './UI/Input.jsx';
+
 export default function Cart(props) {
 	const { cart } = props;
 	const totalCartPrice = cart.reduce((total, cartItem) => {
@@ -7,42 +9,44 @@ export default function Cart(props) {
 	return (
 		<div className='cart-layout'>
 			{cart.length > 0 && (
-				<table className='table table-cart'>
-					<thead>
-						<tr>
-							<th width='25%' className='th-product'>
-								Product
-							</th>
-							<th width='20%'>Unit price</th>
-							<th width='10%'>Quantity</th>
-							<th width='25%'>Total</th>
-						</tr>
-					</thead>
-					<tbody>
-						{cart.map((cartItem, index) => (
-							<tr key={index}>
-								<td>
-									<img width='30' height='30' alt='' src={cartItem.image} />
-									{cartItem.name}
-								</td>
-								<td>€{cartItem.price}</td>
-								<td>{cartItem.quantity}</td>
-								<td>
-									<strong>
-										€{(cartItem.price * cartItem.quantity).toFixed(2)}
-									</strong>
-								</td>
+				<>
+					<table className='table table-cart'>
+						<thead>
+							<tr>
+								<th width='25%' className='th-product'>
+									Product
+								</th>
+								<th width='20%'>Unit price</th>
+								<th width='10%'>Quantity</th>
+								<th width='25%'>Total</th>
 							</tr>
-						))}
-					</tbody>
-					<tfoot>
-						<tr>
-							<th colSpan='2'></th>
-							<th className='cart-highlight'>Total</th>
-							<th className='cart-highlight'>€{totalCartPrice.toFixed(2)}</th>
-						</tr>
-					</tfoot>
-				</table>
+						</thead>
+						<tbody>
+							{cart.map((cartItem, index) => (
+								<tr key={index}>
+									<td>
+										<img width='30' height='30' alt='' src={cartItem.image} />
+										{cartItem.name}
+									</td>
+									<td>€{cartItem.price}</td>
+									<td>{cartItem.quantity}</td>
+									<td>
+										<strong>
+											€{(cartItem.price * cartItem.quantity).toFixed(2)}
+										</strong>
+									</td>
+								</tr>
+							))}
+						</tbody>
+						<tfoot>
+							<tr>
+								<th colSpan='2'></th>
+								<th className='cart-highlight'>Total</th>
+								<th className='cart-highlight'>€{totalCartPrice.toFixed(2)}</th>
+							</tr>
+						</tfoot>
+					</table>
+				</>
 			)}
 
 			{cart.length === 0 && (
